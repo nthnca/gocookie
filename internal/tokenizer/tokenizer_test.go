e2858e69b2c9dedd16f50ef415517864dad1bce4
Nathan Bullock <nathanbullock@gmail.com>
2019-07-26 23:17:03 -0700

    Oops, should be testing externally

--- internal/tokenizer/tokenizer_test.go
+++ internal/tokenizer/tokenizer_test.go
@@ -1,9 +1,11 @@
-package tokenizer
+package tokenizer_test
 
 import (
 	"bytes"
 	"fmt"
 	"testing"
+
+	"github.com/nthnca/gocookie/internal/tokenizer"
 )
 
 func SimpleHarness(tst *testing.T, in string, token int, expected string, ee error) {
@@ -15,8 +17,9 @@ func SimpleHarness(tst *testing.T, in string, token int, expected string, ee err
 		return nil
 	}
 
-	err := CreateTokenizer(bytes.NewReader([]byte(in))).NextToken([]TokenAction{
-		TokenAction{token, callback}})
+	err :=
+		tokenizer.CreateTokenizer(bytes.NewReader([]byte(in))).NextToken(
+			[]tokenizer.TokenAction{tokenizer.TokenAction{token, callback}})
 	if err != nil {
 		if ee == nil {
 			tst.Errorf("Unexpected error: %q", err)
@@ -25,9 +28,9 @@ func SimpleHarness(tst *testing.T, in string, token int, expected string, ee err
 }
 
 func TestIdentToken(tst *testing.T) {
-	SimpleHarness(tst, "   input", IDENT_TOKEN, "input", nil)
+	SimpleHarness(tst, "   input", tokenizer.IDENT_TOKEN, "input", nil)
 }
 
 func TestIdentTokenWithColon(tst *testing.T) {
-	SimpleHarness(tst, "   :in\nas", IDENT_TOKEN, "", fmt.Errorf("hi"))
+	SimpleHarness(tst, "   :in\nas", tokenizer.IDENT_TOKEN, "", fmt.Errorf("hi"))
 }
